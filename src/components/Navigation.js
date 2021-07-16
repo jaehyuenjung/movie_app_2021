47d8c8781f3d8321f6c20e08974517e24bfd8098
jaehyuenjung <jaehyuen0613@gmail.com>
2021-07-16 14:53:14 +0900

    Redirecting

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 function Naviiation() {
     return (
-        <div>
+        <div className="nav-bar">
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
         </div>
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function About(props) {
+function About() {
     return <span>About this page: I built it because I love movies.</span>;
 }
 
